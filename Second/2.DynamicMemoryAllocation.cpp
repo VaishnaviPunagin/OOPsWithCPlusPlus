7cec3d319dbf9ca3fe07ce07e9834b3bdd3cca6a
PercyJackson <dumdumwantsagumgum@gmail.com>
2020-10-03 22:03:15 +0530

    DynamicMemoryAllocationFile!

---
+++ Second/2.DynamicMemoryAllocation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void Area(int side){
+  cout<<"This is a square! Area is "<<side*side<<"\n\n";
+}
+void Area(float radius){
+  cout<<"This is a circle! Area is "<<3.14*radius*radius<<"\n\n";
+}
+void Area(int length, int breadth){
+  cout<<"This is a rectangle! Area is "<<length*breadth<<"\n\n";
+}
+
+int main(){
+  int *where= new int;
+  cout<<"Come on, give us a number!  ";
+  cin>>*where;
+  cout<<"Number stored: "<<*where<<"Pointer Address :"<<where<<"  "<<&where;
+  return 0;
+
+}
