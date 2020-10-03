1a1ee1309652478a3a9b32685a944080f555d0d3
PercyJackson <dumdumwantsagumgum@gmail.com>
2020-10-03 22:12:39 +0530

    Adding the Function Overloading Section

--- Second/2.DynamicMemoryAllocation.cpp
+++ Second/2.DynamicMemoryAllocation.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 void Area(int side){
-  cout<<"This is a square! Area is "<<side*side<<"\n\n";
+  cout<<"\nThis is a square! Area is "<<side*side<<"\n\n";
 }
 void Area(float radius){
-  cout<<"This is a circle! Area is "<<3.14*radius*radius<<"\n\n";
+  cout<<"\nThis is a circle! Area is "<<3.14*radius*radius<<"\n\n";
 }
 void Area(int length, int breadth){
-  cout<<"This is a rectangle! Area is "<<length*breadth<<"\n\n";
+  cout<<"\nThis is a rectangle! Area is "<<length*breadth<<"\n\n";
 }
 
 int main(){
@@ -16,6 +16,14 @@ int main(){
   cout<<"Come on, give us a number!  ";
   cin>>*where;
   cout<<"Number stored : "<<*where<<"\nPointer's Address : "<<where<<"\nAddress of the variable in heap memory : "<<&where<<"\n\n";
+  cout<<"\n--------------------------\nFunction Overloading Demonstration with a function called Area!\n";
+  cout<<"Passing : Area(20);  --> ";
+  Area(20);
+  cout<<"Passing : Area(12.9);  --> ";
+  Area(12.9);
+  cout<<"Passing : Area(12,5);  --> ";
+  Area(12,5);
+  cout<<"\n\nNotice how based on different parameters, the function Area still works! Look at the code to understand how exatly!\n\n"
   return 0;
 
 }
